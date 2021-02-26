e54152b87a64533be4df299d6a193e13d88c7ddb
Vivek shukla <shukla.vivek1993@gmail.com>
2021-02-27 05:16:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+// Set static folder
+app.use(express.static(__dirname));
+//app.use(express.static('public'))
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
